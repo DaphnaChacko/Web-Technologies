9c988afaa8ed651825e35dd426d2c564970e5a11
DaphnaChacko <79188341+DaphnaChacko@users.noreply.github.com>
2021-03-14 00:34:57 +0530

    Update Match.js

--- Match.js
+++ Match.js
@@ -1,3 +1,4 @@
+/***************************XML**************************/
 function loadDoc() {
     
     var xhttp = new XMLHttpRequest();
@@ -23,34 +24,35 @@ function loadDoc() {
     }
     document.getElementById("demo").innerHTML = table;
   }
-
-  function fnCheck(){
-    var xttp = new XMLHttpRequest();
-    xttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        fnPwd(this);
+/***************************JSON************************/
+    function fnCheck(){
+        var xttp = new XMLHttpRequest();
+        xttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          fnPwd(this);
+        }
+       };
+      xttp.open("GET", "http://127.0.0.1/Credentials.json", true);
+      xttp.send();
       }
-    };
-    xttp.open("GET", "http://127.0.0.1/Credentials.json", true);
-    //xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xttp.send();
-  }
-
-  function fnPwd(jsonObj){
-    var jsonDoc = jsonObj.responseText;
-    var myObj=JSON.parse(jsonDoc);
+     function fnPwd(jsonObj){
+      var jsonDoc = jsonObj.responseText;
+      var myObj=JSON.parse(jsonDoc);
     
-    var txtUname=document.getElementById("uid");
-    var txtPwd=document.getElementById("pwd");
-    var i,flag=0;
-    for(i in myObj.login){
+      var txtUname=document.getElementById("uid");
+      var txtPwd=document.getElementById("pwd");
+      var i,flag=0;
+      for(i in myObj.login){
         if(myObj.login[i].uname == txtUname.value){
             flag=0;
             if(myObj.login[i].pwd == txtPwd.value){
                 alert("Login success");
+                break;
             }
             else{
+              alert(myObj.login[i].pwd);
                 alert("Invalid Password");
+                break;
             }
         }
         else{
